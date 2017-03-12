f94b305b635ebf7515104422a5a9eaaf0c15f42a
qihuiyang <qyang19@ucsc.edu>
2017-03-11 23:35:05 -0800

    add split function so that the total amount can be split into different number of persons.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -14,20 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet var tipLabel: UILabel!
     @IBOutlet var totalLabel: UILabel!
     @IBOutlet var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipEach: UILabel!
+    @IBOutlet weak var personSlider: UISlider!
+    
     
     let tipPercentages:Array = [0.10, 0.15, 0.18, 0.20, 0.25]
     let defaults = UserDefaults.standard
     
+    var person = "1"
     
     override func viewWillAppear(_ animated: Bool) {
         billField.center.y -= view.bounds.width
         tipLabel.center.y -= view.bounds.width
         totalLabel.center.y -= view.bounds.width
+        tipEach.center.y -= view.bounds.width
         
         UIView.animate(withDuration: 0.8, animations: {
             self.billField.center.y += self.view.bounds.width
             self.tipLabel.center.y += self.view.bounds.width
             self.totalLabel.center.y += self.view.bounds.width
+            self.tipEach.center.y += self.view.bounds.width
+            
         })
         
     }
@@ -89,9 +96,14 @@ class ViewController: UIViewController {
         let tipPercentages = [0.10, 0.15, 0.18, 0.2, 0.25]
         
         let bill = Double(billField.text!) ?? 0
+        person = String(format: "%.0f",personSlider.value)
+        
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
+        let perNum = Double(person) ?? 1
+        let eachBill = total / perNum
         
+        tipEach.text = person + " x " + String(format: "$%.2f", eachBill)
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
         
